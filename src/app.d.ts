64d8f4ff9d33eb90babddb6e3635e2e78478d3fb
= <r.gnzlz.cstll@gmail.com>
2022-06-01 22:17:31 -0400

    adding bind to database

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,7 +4,15 @@
 // for information about these interfaces
 declare namespace App {
 	// interface Locals {}
-	// interface Platform {}
+	interface Platform {
+		env: {
+			SDOS: { binding: "SDOS", id: "bda7e46611f84c6f8372502a588c3abe" }
+		};
+		context: {
+			waitUntil(promise: Promise<any>): void;
+		}
+	}
+
 	// interface Session {}
 	// interface Stuff {}
 }
