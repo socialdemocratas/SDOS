fc00258013afda3330552eedbf70e2abd08c41bd
= <r.gnzlz.cstll@gmail.com>
2022-06-01 23:18:06 -0400

    test

--- src/routes/[org_slug=orgList]/[...slug=page]/index.js
+++ src/routes/[org_slug=orgList]/[...slug=page]/index.js
@@ -26,8 +26,7 @@ export async function get({ params, platform }) {
     if (page === null) {
         return {
             body: {
-                no_found: `${org_slug}/page/${slug}`,
-                ...fakeDb['']
+                no_found: `${org_slug}/page/${slug}`
             }
         };
     }
